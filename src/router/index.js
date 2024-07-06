83d9da191430a91ac56e185fb3e4c4e33f3e9368
vinyzada <vinyescolas@gmail.com>
2024-07-05 22:35:12 -0300

    FIX: deleted routers view

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'CadUser',
-      component: () => import('../views/cad-user.vue')
-    },
-    {
-      path: '/perfil',
-      name: 'PerfUser',
-      component: () => import('../views/perf-user.vue'),
-    }
   ] 
 })
 
